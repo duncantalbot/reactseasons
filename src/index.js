127969281e0ba2423623bc1a895f3e14917b1545
Duncan Talbot <duncan_talbot@hotmail.com>
2019-02-11 22:24:29 +1100

    State Final

--- src/index.js
+++ src/index.js
@@ -11,23 +11,30 @@ class App extends React.Component {
         super(props);
 
         //THIS IS THE ONLY TIME we do direct assigment to this.state
-        this.state = {lat: null };
+        this.state = {lat: null, errorMessage: ''};
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({lat : position.coords.latitude});
             },
-            (err) => console.log(err)
+            (err) => {
+                this.setState({errorMessage : err.message});
+            }
         );
-
     }
 
     //React says we have to define render methid.
     render() {
-        return (
-            <div>Latitude: {this.state.lat}</div>
-        );
-    }
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div>Lattitude: {this.state.lat}</div>
+        }
+
+            return <div>Loading!</div>
+    };
 }
 
 ReactDOM.render(
